a537870543b4d477b7a3dbbcc5af9a25d8779bfb
Inacio Filho <inacioboueres@gmail.com>
2016-07-15 21:49:11 -0300

    Rum in tomcat

--- src/main/java/com/inacio/boueres/fizz/buzz/Application.java
+++ src/main/java/com/inacio/boueres/fizz/buzz/Application.java
@@ -2,14 +2,24 @@ package com.inacio.boueres.fizz.buzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.inacio.boueres.fizz.buzz")
 @EnableAutoConfiguration
-public class Application {
-	public static void main(String[] args) {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
 }
